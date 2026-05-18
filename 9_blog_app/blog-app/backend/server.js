d101e573982c2f0f4de48dd8c74805646d0d69a5
StrungPattern-coder <ksriram4584@gmail.com>
2026-05-19 03:20:21 +0530

    add additional allowed origin for CORS

--- 9_blog_app/blog-app/backend/server.js
+++ 9_blog_app/blog-app/backend/server.js
@@ -12,7 +12,8 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5174",
-    "http://127.0.0.1:5174"
+    "http://127.0.0.1:5174",
+    "http://13.61.27.68:5173"
 ];
 
 const corsOptions = {
